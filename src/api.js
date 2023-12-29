f10123f7b6c822827161acdc261f9f3acb098462
Rajveer singh <rajveerara01@gmail.com>
2023-12-29 19:14:17 +0530

    initial commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: "GET",
+	headers: {
+	  "X-RapidAPI-Key": "2bfaf95fb3msh6ee234376176d09p1ea0a2jsn66c24be39a52",// enter your rapid api key here
+	  "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "9d72155c3f91b56db631bfdadde01ab1";
---
+++ src/components/Search/index.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { geoApiOptions, GEO_API_URL } from "../../api";
+
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
+
+  const loadOptions = async (inputValue, loadedOptions) => {
+    try {
+      const response = await fetch(
+        `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+        geoApiOptions
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+
+      return {
+        options: data.data.map((city) => ({
+          value: `${city.latitude} ${city.longitude}`,
+          label: `${city.name}, ${city.countryCode}`,
+        })),
+        hasMore: data.data.length === 10, // Adjust the condition based on your API response
+      };
+    } catch (error) {
+      console.error("Error loading options:", error);
+      return {
+        options: [],
+        hasMore: false,
+      };
+    }
+  };
+
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+
+  return (
+    <AsyncPaginate
+      placeholder="Search for city"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+  );
+};
+
+export default Search;
